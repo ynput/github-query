5257cf35f5b0cdc2f56f8d19c7ea155ae83cd771
PhilNewm <Philippkd8@gmail.com>
2024-10-18 09:41:49 +0200

    Small refactor
    Enabled cli output

--- github_query.py
+++ github_query.py
@@ -1,38 +1,41 @@
 """
 This script is written in a certain - maybe even unconventional way - by intention.
-It's supposed to be run from comamndline right away to be used in a github action workflow yaml file.
-Additonally it's test suite relies mainly on putest and therefore the functions need to be importable to the pytest script.
+It's supposed to be run from command line right away to be used in a github action workflow yaml file.
+Additionally it's test suite relies mainly on pytest and therefore the functions need to be importable to the pytest script.
 """
 
-import argparse
 import json
 import logging
 import re
 import subprocess
 
 from collections import namedtuple
-from itertools import groupby
 
 
 logger = logging.getLogger(__name__)
 
-def parse_args() -> dict:
-    """Parse command-line arguments and store them in a global variable."""
+Changelog = namedtuple("Changelog", "labels title number url id")
 
-    parser = argparse.ArgumentParser(description="A python script to convert GitHub PR information to a more simple format.")
-    parser.add_argument("repo", type=str, help="Repository name consisting of 'repo-owner/repo-name'")
-    parser.add_argument("query_parameters", type=str, help="Keys to query for.")
-    parser.add_argument("date", type=str, default="2024-07-08T09:48:33Z", help="Latest release date.")
-    parsed_args = parser.parse_args()
 
-    repo_name = parsed_args.repo
-    query_tags = parsed_args.query_parameters.split(',')
-    latest_release_date = parsed_args.date
 
-    command = f"gh pr list --state merged --search 'merged:>={latest_release_date}' --json {','.join(query_tags)} --repo {repo_name}"
-    pr_json = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# def parse_args() -> dict:
+#     """Parse command-line arguments and store them in a global variable."""
 
-    return json.loads(pr_json.stdout)
+#     parser = argparse.ArgumentParser(description="A python script to convert GitHub PR information to a more simple format.")
+#     parser.add_argument("repo", type=str, help="Repository name consisting of 'repo-owner/repo-name'")
+#     parser.add_argument("query_parameters", type=str, help="Keys to query for.")
+#     parser.add_argument("date", type=str, default="2024-07-08T09:48:33Z", help="Latest release date.")
+#     parsed_args = parser.parse_args()
+
+#     repo_name = parsed_args.repo
+#     query_tags = parsed_args.query_parameters.split(',')
+#     latest_release_date = parsed_args.date
+
+#     command = f"gh pr list --state merged --search 'merged:>={latest_release_date}' --json {','.join(query_tags)} --repo {repo_name}"
+#     pr_json = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+#     return json.loads(pr_json.stdout)
+
 
 def get_changelog(pr_data, changelog_start="## Changelog", heading="##"):
     """Get list of changes from a PRs changelog.
@@ -63,24 +66,25 @@ def get_changelog(pr_data, changelog_start="## Changelog", heading="##"):
 
     return changelog_lines
 
-def filter_changes_per_label(pr_data: dict, changelog_label_list: list) -> namedtuple:
-    
-    Changelog = namedtuple("Changelog", "labels title number url id")
-    changes_list = []
+
+def filter_changes_per_label(pr_data: list[dict[str, str]], changelog_label_list: list[str]) -> list[Changelog]:
+
+    changes_list: list[Changelog] = []
 
     for pull_request in pr_data:
 
         # TODO refactor this to become more readable
-        label_list = [label["name"] for label in pull_request["labels"] if label["name"] in changelog_label_list]
+        label_list: list[str] = [label["name"] for label in pull_request["labels"] if label["name"] in changelog_label_list]
         if label_list:
             changes_list.append(Changelog(label_list, pull_request["title"], pull_request["number"], pull_request["url"], pull_request["id"]))
 
     return changes_list
 
-def sort_changes(changes_list: namedtuple, changelog_label_list: list) -> namedtuple:
+
+def sort_changes(changes_list: list[Changelog], changelog_label_list: list[str]) -> list[Changelog]:
 
     # TODO implement this logic in a more clever way
-    sorted_changes = []
+    sorted_changes: list[Changelog] = []
     for order_label in changelog_label_list:
         for change in changes_list:
             if any(label == order_label for label in change.labels):
@@ -88,13 +92,14 @@ def sort_changes(changes_list: namedtuple, changelog_label_list: list) -> namedt
 
     return sorted_changes
 
-def build_changelog_markdown(changes):
+
+def build_changelog_markdown(changes: list[Changelog]) -> str:
     changelog = "# Changelog"
-    previous_labels = []
+    previous_labels: list[str] = []
 
     # TODO implement this logic in a more clever way, checkout `from itertools import groupby`
     for change in changes:
-        current_labels = change.labels
+        current_labels: list[str] = change.labels
         
         if not any(label in previous_labels for label in current_labels):
             changelog += f"\n\n## {change.labels[0]}\n\n"
@@ -106,7 +111,7 @@ def build_changelog_markdown(changes):
     return changelog
 
 
-def get_labels(pr_data: dict) -> list:
+def get_labels(pr_data: list[dict[str, str]]) -> list[str]:
     """Filter all unique labels from dictionary.
 
     Args:
@@ -129,7 +134,8 @@ def get_labels(pr_data: dict) -> list:
 
     return list(labels)
 
-def get_repo_var(repo: str, var_name: str) -> list:
+
+def get_repo_var(repo: str, var_name: str) -> list[str]:
     """Query labels from repository variables.
 
     Args:
@@ -148,13 +154,15 @@ def get_repo_var(repo: str, var_name: str) -> list:
 
     return csv_string_to_list(labels.stdout)
 
-def csv_string_to_list(input: str) -> list:
+
+def csv_string_to_list(input: str) -> list[str]:
     if input:
         return re.split(r',\s*', input.strip())
 
     return []
 
-def get_version_increment(patch_bump_list: list, minor_bump_list: list, pr_label_list: list):
+
+def get_version_increment(patch_bump_list: list[str], minor_bump_list: list[str], pr_label_list: list[str]):
     """Figure out version increment based on PR labels.
 
     Args:
@@ -178,15 +186,20 @@ def get_version_increment(patch_bump_list: list, minor_bump_list: list, pr_label
 
     return ""
 
-if __name__ == '__main__':
 
-    # TODO remove before merging
-    command = f"gh pr list --state merged --search 'merged:>=2024-08-01T11:29:22Z' --json body,labels,title,number,url,id --repo ynput/ayon-maya"
+def generate_release_changelog() -> str:
+    command: str = f"gh pr list --state merged --search 'merged:>=2024-08-01T11:29:22Z' --json body,labels,title,number,url,id --repo ynput/ayon-maya"    
     pr_json = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    pr_data = json.loads(pr_json.stdout)
+    pr_data: str = json.loads(pr_json.stdout)
+    changelog_labels: list[str] = ["type: bug", "type: enhancement", "type: maintenance"]
 
-    changelog_labels = ["type: bug", "type: enhancement", "type: maintenance"]
-    pr_filtered = filter_changes_per_label(pr_data=pr_data, changelog_label_list=changelog_labels)
-    sorted_changes = sort_changes(changes_list=pr_filtered, changelog_label_list=changelog_labels)
-    markdown_changelog = build_changelog_markdown(sorted_changes)
+    pr_filtered: Changelog = filter_changes_per_label(pr_data=pr_data, changelog_label_list=changelog_labels)
+    sorted_changes: list[Changelog] = sort_changes(changes_list=pr_filtered, changelog_label_list=changelog_labels)
+    markdown_changelog: str = build_changelog_markdown(sorted_changes)
+
     print(markdown_changelog)
+
+    return markdown_changelog
+
+if __name__ == "__main__":
+    generate_release_changelog()
