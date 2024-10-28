3dba933e21622955b39a1b748e0f2e6d28633b26
PhilNewm <Philippkd8@gmail.com>
2024-10-28 09:44:48 +0100

    remove variable query

--- github_query.py
+++ github_query.py
@@ -210,7 +210,7 @@ def generate_release_changelog(latest_release_date: str, query_tags: str, repo_n
 
     query_tags_list: list[str] = conversion_logic.csv_string_to_list(query_tags)
     pr_result: list[dict[str, str]] = queries.query_merged_prs(latest_release_date, query_tags_list, repo_name)
-    changelog_markdown_result: list[str] = conversion_logic.csv_string_to_list(queries.get_repo_var(repo=repo_name, var_name=changelog_labels))
+    changelog_markdown_result: list[str] = conversion_logic.csv_string_to_list(changelog_labels)
 
     pr_filtered: list[Changelog] = filter_changes_per_label(pr_data=pr_result, changelog_label_list=changelog_markdown_result)
     sorted_changes: list[Changelog] = sort_changes(changes_list=pr_filtered, changelog_label_list=changelog_labels)
